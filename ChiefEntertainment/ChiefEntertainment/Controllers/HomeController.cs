e0142429dc7e84483f14f72f2271958dbd006204
Gwendolyn Zubatch <gyzubatch@gmail.com>
2019-05-08 10:54:08 -0700

    added content in Calendar, Contact, Booking; added email service for Contact form

--- ChiefEntertainment/ChiefEntertainment/Controllers/HomeController.cs
+++ ChiefEntertainment/ChiefEntertainment/Controllers/HomeController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ChiefEntertainment.Models;
+using ChiefEntertainment.Models.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -61,6 +63,44 @@ namespace ChiefEntertainment.Controllers
         public IActionResult Contact()
         {
             return View();
+        }
+
+        [AllowAnonymous]
+        [Route("SubmitContact")]
+        public async Task<IActionResult> SubmitContact(ContactViewModel vm)
+        {
+                // build email
+                Email message = new Email()
+                {
+                    Recipient = User.Identity.Name,
+                    ConfigSet = "",
+                    Subject = "Message received on chief-entertainment.net",
+                    BodyHtml = $@"<html>
+                                <head></head>
+                                <body>
+                                  <h3>Message from: {vm.Name}</h3>
+                                  <h3>Contact email: {vm.Email}</h3>
+                                  <h3>Contact phone: {vm.Phone}</h3>
+                                  <h3>Message:</h3>
+                                  <p>{vm.Message}</p>
+                                </body>
+                                </html>",
+                };
+
+                // send email (3 tries)
+                int counter = 0;
+                bool emailStatus = false;
+                while (emailStatus == false && counter < 3)
+                {
+                    emailStatus = await Email.Send(message);
+                }
+
+                if (emailStatus == false)
+                {
+                    ModelState.AddModelError(string.Empty, "Message failed. Please try again.");
+                    return View("Contact", vm);
+                }
+                return RedirectToRoute("");
         }
     }
 }
